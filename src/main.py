19fa099b90fd31918924b80b643ed664f49a367a
tokudaek <tokudaek@gmail.com>
2020-11-05 22:54:34 -0300

    Upate

--- src/main.py
+++ src/main.py
@@ -12,48 +12,79 @@ import sys
 import numpy as np
 import matplotlib; matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+import igraph
 from myutils import info, create_readme
+import random
 
+#############################################################
+NONE = 0
+NUCLEUS = 1
+RESOURCE = 2
+#############################################################
+UNIFORM = 0
+DEGREE = 1
 #############################################################
 def generate_graph(model, nvertices, avgdegree, rewiringprob,
                    latticethoroidal=False, tmpdir='/tmp/'):
     """Generate graph with given topology """
+    info(inspect.stack()[0][3] + '()')
 
-    if topologymodel == 'la':
+    if model == 'la':
         mapside = int(np.sqrt(nvertices))
         g = igraph.Graph.Lattice([mapside, mapside], nei=1, circular=latticethoroidal)
-    elif topologymodel == 'er':
+    elif model == 'er':
         erdosprob = avgdegree / nvertices
         if erdosprob > 1: erdosprob = 1
         g = igraph.Graph.Erdos_Renyi(nvertices, erdosprob)
-    elif topologymodel == 'ba':
+        breakpoint()
+        
+    elif model == 'ba':
         m = round(avgdegree/2)
         if m == 0: m = 1
         g = igraph.Graph.Barabasi(nvertices, m)
-    elif topologymodel == 'ws':
+    elif model == 'ws':
         mapside = int(np.sqrt(nvertices))
         m = round(avgdegree/2)
-        g = igraph.Graph.Lattice([mapside, mapside], nei=1,
-                                 circular=False)
+        g = igraph.Graph.Lattice([mapside, mapside], nei=1, circular=False)
         g.rewire_edges(rewiringprob)
-    elif topologymodel == 'gr':
+    elif model == 'gr':
         radius = get_rgg_params(nvertices, avgdegree)
         g = igraph.Graph.GRG(nvertices, radius)
+    else:
+        msg = 'Please choose a proper topology model'
+        raise Exception(msg)
 
     g = g.clusters().giant()
 
-    if topologymodel in ['gr', 'wx']:
+    if model in ['gr', 'wx']:
         aux = np.array([ [g.vs['x'][i], g.vs['y'][i]] for i in range(g.vcount()) ])
         # layoutmodel = 'grid'
     else:
-        if topologymodel in ['la', 'ws']:
+        if model in ['la', 'ws']:
             layoutmodel = 'grid'
         else:
             layoutmodel = 'random'
         aux = np.array(g.layout(layoutmodel).coords)
     # coords = (aux - np.mean(aux, 0))/np.std(aux, 0) # standardization
     coords = -1 + 2*(aux - np.min(aux, 0))/(np.max(aux, 0)-np.min(aux, 0)) # minmax
+    g.vs['type'] = NONE
     return g, coords
+
+##########################################################
+def add_labels(gorig, m, choice, label):
+    """Add @nresources to the @g.
+    We randomly sample the vertices and change their labels"""
+    info(inspect.stack()[0][3] + '()')
+    # TODO: use @choice
+    g = gorig.copy()
+    types = np.array(g.vs['type'])
+    nones = np.where(types == NONE)[0]
+    random.shuffle(nones)
+
+    for i in range(m):
+        idx = nones[i]
+        g.vs[idx]['type'] = label
+    return g, sorted(nones[:m])
 
 ##########################################################
 def main():
@@ -68,23 +99,42 @@ def main():
     readmepath = create_readme(sys.argv, args.outdir)
 
     np.random.seed(args.seed)
-    models = ['ER'] # ['ER', 'BA']
-    nvertices = 100
-    nedges = 200
-    nresources = nvertices - nnuclei # To reproduce Luc's res., use nvert-nnuclei
-    cs = [.25] # np.arange(0, 1.01, .2)
-    rewiringprob = 0.5
-    avgdegree = 10
+    random.seed(args.seed)
 
+    models = ['er'] # ['ER', 'BA']
+    nvertices = 100
+    resoratio = .5
+    nresources = int(resoratio * nvertices)
+    nucleiratios = [.5] # np.arange(0, 1.01, .2)
+    rewiringprob = 0.5
+    avgdegree = 5
+    niter = 10
+
+    res = []
     for model in models:
-        g = generate_graph(model, nvertices, avgdegree, rewiringprob)
-        g = add_resources(g, nresources)
-        for c in cs:
+        gorig, coords = generate_graph(model, nvertices, avgdegree, rewiringprob)
+        gorig, resoids = add_labels(gorig, nresources, UNIFORM, RESOURCE)
+
+        for c in nucleiratios:
             nnuclei = int(c * nvertices)
             for i in range(niter):
-                gnew = add_nuclei(g, nnuclei)
-                r = neighnvert / nvertices
-                s = neighnvert / neighnedges
+                neighs = []
+                g, nuclids = add_labels(gorig, nnuclei, UNIFORM, NUCLEUS)
+                for resoid in resoids:
+                    # neighids = g.neighbors(resoid)
+                    neighids = np.array(g.neighbors(resoid))
+                    neightypes = np.array(g.vs[neighids.tolist()]['type'])
+                    localids = np.where(neightypes == RESOURCE)[0]
+                    neighs.extend(neighids[localids])
+
+                lenunique = len(set(neighs))
+                lenrepeated = len(neighs)
+                r = lenunique / nvertices
+                s = lenunique / lenrepeated
+                res.append([model, c, i, r, s])
+
+    print(res)
+    breakpoint()
 
     info('For Aiur!')
 
